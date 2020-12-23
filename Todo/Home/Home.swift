06e572c2caf3897fe049ef11fac742795987af55
Ignatio <ignatio.julian@ti.ukdw.ac.id>
2020-12-24 00:24:58 +0800

    add tableview in home

--- Todo/Home/Home.swift
+++ Todo/Home/Home.swift
@@ -7,15 +7,45 @@
 
 import UIKit
 
-class Home: UIViewController {
+class Home: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
-
-
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(Cells.self, forCellReuseIdentifier: "cellId")
+        
+        view.addSubview(tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 2
+        //        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! Cells
+        //        cell.backgroundColor = UIColor.black
+        //        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! Cells
+        cell.backgroundColor = UIColor.white
+        cell.dayLabel.text = "Day \(indexPath.row+1)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
 }
+
 
