de09462d9d332c65534449231334891ad8a1d7d5
Ignatio <ignatio.julian@ti.ukdw.ac.id>
2020-12-25 21:31:15 +0800

    add boarding screen + config signup - login to firebase

--- Todo/Home/LoginViewController.swift
+++ Todo/Home/LoginViewController.swift
@@ -15,26 +15,31 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
+    
     override func viewDidLoad() {
         //
     }
     
+   
+    
     @IBAction func login(_ sender: Any) {
+        
         let email = emailTextField.text!
         let password = passwordTextField.text!
         
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
-          guard let strongSelf = self else { return }
+            guard let strongSelf = self else { return }
             if(error != nil){
                 strongSelf.statusLabel.text = "Ups! 😟"
                 strongSelf.statusLabel.textColor = .red
-        
+                
                 return
             }
             strongSelf.statusLabel.text = "Enjoy 🥳"
             strongSelf.statusLabel.textColor = .blue
+        
+            self?.dismiss(animated: false, completion: nil)
         }
         
     }
-    
 }
